755d4d5c92d19f7f623a4316e10e3fa855f08638
ramesh <ramesh@fundsindia.com>
2017-09-22 12:52:11 +0530

    Add Price sorting
    Signed-off-by: ramesh <ramesh@fundsindia.com>

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,12 +5,31 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   
-  transform(items: any, filtertext: any): any {
-   if(filtertext){
-       return items.filter( item => item.name.toLowerCase().includes(filtertext.toLowerCase()));
-    }else{
-      return items;
-    }
-  }
+  transform(items, args): any {
+
+    let [filtertext , age] = args;
+    console.log(filtertext)
+    console.log(parseInt(age));
+
+    if(filtertext){
+        return items.filter( function(item){
+              var title = item.title.toLowerCase().includes(filtertext.toLowerCase());
+              var author = item.author.toLowerCase().includes(filtertext.toLowerCase());
+              var email = item.email.toLowerCase().includes(filtertext.toLowerCase());
+              var url = item.URL.toLowerCase().includes(filtertext.toLowerCase());
+              return title || author || email || url;
+        });
+      }
+      if(age){
+        return items.filter(person => {
+          return person.age >= 0 && person.age <= +age;
+        });
+      }
+      else{
+        return items;
+      }
+   }
+
+
 
 }
